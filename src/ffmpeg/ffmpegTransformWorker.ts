dcdb76872a77388a9e2c800e96df8b754b2696f4
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-05-09 20:26:56 +0200

    develop: FFmpegTransformWorker

--- src/ffmpeg/ffmpegTransformWorker.ts
+++ src/ffmpeg/ffmpegTransformWorker.ts
@@ -74,7 +74,7 @@ parentPort.on('message', async (value: FFmpegTransformArgs) => {
     parentPort.postMessage({
       type : 'ffmpegTransform',
       value: result,
-    })
+    }, [result])
   } catch (err) {
     parentPort.postMessage({
       type : 'ffmpegTransform',
